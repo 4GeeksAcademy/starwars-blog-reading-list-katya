b2bbba4a3e46284e823536077dfa7921a1887109
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2024-01-26 20:43:36 +0000

    Navbar done

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,16 +2,49 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+  return (
+    <nav
+      className="navbar bg-dark border-bottom border-body"
+      data-bs-theme="dark"
+    >
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img
+            src="https://cronkitenews.azpbs.org/wp-content/uploads/2021/07/navajo-star-wars-logo.png"
+            alt="Logo"
+            style={{ height: "40px" }}
+            className="d-inline-block align-text-top img-thumbnail ms-5"
+          />
+        </a>
+        <div className="btn-group dropstart me-5">
+          <button
+            type="button"
+            className="btn btn-warning dropdown-toggle"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            My favorites
+          </button>
+          <ul className="dropdown-menu border-0">
+            <ul className="list-group border-0">
+              <p className="m-1 p-2 fs-6 text-warning">Characters</p>
+              <li className="list-group-item p-0 rounded-0">
+                <a className="dropdown-item" href="#">
+                  Example of a character
+                </a>
+              </li>
+            </ul>
+            <ul className="list-group border-0">
+              <p className="m-1 p-2 fs-6 text-warning">Planets</p>
+              <li className="list-group-item p-0 rounded-0">
+                <a className="dropdown-item" href="#">
+                  Example of a planets
+                </a>
+              </li>
+            </ul>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
